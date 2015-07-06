3a2f6f9748ad67c4a9a20c7c21a603a4571afd03
Alex Weber <alexweber15@gmail.com>
2015-07-06 19:08:15 -0300

    wip snippet add/edit form

--- src/form/form.js
+++ src/form/form.js
@@ -1,7 +1,25 @@
 export class SnippetForm {
   heading = 'Add Snippet';
 
-  activate () {
+  // Available language options.
+  languages = [];
 
+  // Default form values.
+  title = '';
+  language = '';
+  tags = '';
+  contents = '';
+
+  activate () {
+    // @TODO figure out if edit form and update default values.
+    // @TODO load available languages.
+    this.languages.push({
+      id: 1,
+      name: 'JavaScript'
+    });
+    this.languages.push({
+      id: 2,
+      name: 'PHP'
+    });
   }
 }
