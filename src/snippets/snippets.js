b947e175a229128743202f803f6384fea830ba17
Alex Weber <alexweber15@gmail.com>
2015-07-06 22:16:10 -0300

    load snippets from db in snippets module

--- src/snippets/snippets.js
+++ src/snippets/snippets.js
@@ -1,10 +1,18 @@
+import models from '../db/models';
+
 export class Snippets {
   heading = 'My Snippets';
   snippets = [];
 
   activate() {
-    // this.snippets.push({
-    //   title: 'ellooo'
-    // });
+    models.Snippet.forge().fetchAll().then((results) => {
+      if (results.length) {
+        results.models.forEach((item) => {
+          this.snippets.push(item.attributes);
+        });
+      }
+    }).catch(function (error) {
+      console.error(error);
+    });
   }
 }
