a1bee308f59e466d2088aa6cd4660ee9f4af43ba
Alex Weber <alexweber15@gmail.com>
2015-07-10 21:47:31 -0300

    add language operations custom element

---
+++ src/language-list/operations.js
@@ -0,0 +1,14 @@
+import {customElement, bindable} from 'aurelia-framework';
+
+@customElement('language-operations')
+export class LanguageOperations {
+  @bindable lang;
+
+  doEdit() {
+    // this.lang.id
+  }
+
+  doDelete() {
+    // this.lang.id
+  }
+}
