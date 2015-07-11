6aaf7420f54c35a9bc90099bad94f6fe02fef1b6
Alex Weber <alexweber15@gmail.com>
2015-07-11 17:15:27 -0300

    rework sass organization

--- build/paths.js
+++ build/paths.js
@@ -7,7 +7,7 @@ module.exports = {
   root: appRoot,
   source: appRoot + '**/*.js',
   html: appRoot + '**/*.html',
-  sass: appRoot + '**/*.scss',
+  sass: './scss/styles.scss',
   output: outputRoot,
   doc:'./doc',
   e2eSpecsSrc: 'test/e2e/src/*.js',
