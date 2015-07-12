704c4083431bdfa36e165a8582059f2ca256f09d
Alex Weber <alexweber15@gmail.com>
2015-07-12 19:05:56 -0300

    refactor snippet helper class

--- src/snippet/snippet.js
+++ src/snippet/snippet.js
@@ -47,32 +47,84 @@ export class Snippet {
       let snippets = [];
       if (results.length) {
         results.models.forEach(item => {
-          snippets.push(item.attributes);
+          let snippet = Snippet._buildSnippet(item);
+          if (snippet) {
+            snippets.push(snippet);
+          }
         });
       }
       return snippets;
     });
   }
 
+  /**
+   * Helper method to preprocess a Snippet bookshelf model and return a plain object
+   * for use in view models.
+   *
+   * @param  {Snippet} snippet A Snippet bookshelf model object.
+   * @return {mixed} A snippet information plain object or null.
+   *
+   * @see Snippet._getLanguage()
+   * @see Snippet._getTags()
+   */
+  static _buildSnippet(snippet) {
+    let retVal = null;
+    if (snippet) {
+      let attrs = snippet.attributes;
+      retVal = {};
+      retVal.id = attrs.id;
+      retVal.title = attrs.title;
+      retVal.contents = attrs.contents;
+      retVal.language = Snippet._getLanguage(snippet);
+
+      // @TODO fetch & preprocess tags.
+      // retVal.tags = [];
+    }
+    return retVal;
+  }
+
+  /**
+   * Helper method to return language information for a snippet.
+   *
+   * @param  {Snippet} snippet A Snippet bookshelf model object.
+   * @return {mixed} A language information plain object if available or null.
+   */
+  static _getLanguage(snippet) {
+    let retVal = null;
+    let language = snippet.related('language');
+    if (language.id) {
+      retVal = {
+        id: language.id,
+        name: language.attributes.name,
+        code: language.attributes.code,
+      };
+    }
+    return retVal;
+  }
+
+  /**
+   * Helper method to return tags information for a snippet.
+   *
+   * @param  {Snippet} snippet A Snippet bookshelf model object.
+   * @return {mixed} An array of tag information plain objects available or null.
+   */
+  static _getTags(snippet) {
+    let retVal = null;
+    let tags = snippet.related('tags');
+    // @TODO figure this out when we have actual tag data saved.
+    return retVal;
+  }
+
+  /**
+   * Load a snippet by id.
+   *
+   * @param  {integer} id A snippet id.
+   * @return {Promise} A promise for a snippet plain object containg language and tags
+   *                   info as plain objects if available.
+   */
   static load(id) {
     return models.Snippet.forge({ id }).fetch({ withRelated: ['language'] }).then(snippet => {
-      let retVal;
-      if (snippet) {
-        let attrs = snippet.attributes;
-        retVal = {};
-        retVal.id = attrs.id;
-        retVal.title = attrs.title;
-        // retVal.tags = attrs.tags;
-        retVal.tags = [];
-        retVal.contents = attrs.contents;
-        retVal.language = null;
-
-        let language = snippet.related('language');
-        if (language.id) {
-          retVal.language = language.id.toString();
-        }
-      }
-      return retVal;
+      return Snippet._buildSnippet(snippet);
     });
   }
 }
